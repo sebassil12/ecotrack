1d7c4a9499ff13b042ed8a9c294e5cb77a05a56b
sebassil12 <anddysilva15@gmail.com>
2025-07-07 21:25:45 -0400

    first commit

---
+++ src/main/java/com/ecotrack/ecotrack/EcotrackApplication.java
@@ -0,0 +1,13 @@
+package com.ecotrack.ecotrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcotrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcotrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecotrack/ecotrack/EcotrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecotrack.ecotrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcotrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
