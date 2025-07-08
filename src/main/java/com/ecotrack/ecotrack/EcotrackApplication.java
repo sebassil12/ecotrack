e7e2276f6a23f24bbe0f52df9a44e6e49ba05ec2
Saul Torres <Saul Torres@DESKTOP-6U11T9F>
2025-07-07 21:07:59 -0600

    testeo

--- src/main/java/com/ecotrack/ecotrack/EcotrackApplication.java
+++ src/main/java/com/ecotrack/ecotrack/EcotrackApplication.java
@@ -1,6 +1,5 @@
 package com.ecotrack.ecotrack;
-//hola mundo
-//prueba dos 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
