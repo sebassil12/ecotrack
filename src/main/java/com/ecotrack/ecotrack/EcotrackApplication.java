cbe5b0375eec36409837e833151ad760f559ea9b
Tu Nombre <tu@email.com>
2025-07-07 21:12:14 -0500

    prueba 1 bm

--- src/main/java/com/ecotrack/ecotrack/EcotrackApplication.java
+++ src/main/java/com/ecotrack/ecotrack/EcotrackApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EcotrackApplication {
+	
+	// prueba 1 bm
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcotrackApplication.class, args);
